565c8d1658f15be3831028a8cddff96b656bc54c
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-06-22 09:30:30 -0700

    submission for deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([])
+  const [filter, setFilter] = useState("");
+  const bankAPI = "http://localhost:8001/transactions"
+
+  useEffect(() => {
+    fetch(bankAPI)
+    .then(resp => resp.json())
+    .then(newTransactions => {
+      setTransactions(() => newTransactions)
+    })
+  }, [])
+
+  function addNewTransaction(newTransaction){
+    setTransactions(() => [...transactions, newTransaction])
+  }
+
+  function handleSearchChange(search){
+    setFilter(search)
+  }
+
+  const transactionsToDisplay = transactions.filter((transaction) => {
+    if(filter === ""){
+      return true;
+    } else {
+      return (transaction.description.includes(filter))
+    }
+  })
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search handleSearchChange={handleSearchChange}/>
+      <AddTransactionForm bankAPI={bankAPI} addNewTransaction={addNewTransaction}/>
+      <TransactionsList transactions={transactionsToDisplay} />
     </div>
   );
 }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,9 +1,31 @@
 import React from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({bankAPI, addNewTransaction}) {
+  function handleSubmit(event){
+    event.preventDefault();
+
+    fetch(bankAPI, {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        date: event.target.date.value,
+        description: event.target.description.value,
+        category: event.target.category.value,
+        amount: event.target.amount.value
+      })
+    })
+      .then(resp => resp.json())
+      .then(newTransaction => {
+        event.target.description.value = '';
+        event.target.category.value = ''
+        event.target.amount.value = ''
+        addNewTransaction(newTransaction)
+      })
+  }
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
           <input type="date" name="date" />
           <input type="text" name="description" placeholder="Description" />
